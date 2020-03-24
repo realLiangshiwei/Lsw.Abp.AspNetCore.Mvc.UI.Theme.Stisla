723a98b09ef903abc7f885e39eedf36056566587
liangshiwei <liangshiw@outlook.com>
2020-03-24 10:54:37 +0800

    Resolved #12 : Add pagealerts component

---
+++ src/Themes/Stisla/Components/PageAlerts/PageAlertsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Alerts;
+
+namespace Lsw.Abp.AspNetCore.Mvc.UI.Theme.Stisla.Themes.Stisla.Components.PageAlerts
+{
+    public class PageAlertsViewComponent : AbpViewComponent
+    {
+        private readonly IAlertManager _alertManager;
+
+        public PageAlertsViewComponent(IAlertManager alertManager)
+        {
+            _alertManager = alertManager;
+        }
+
+        public IViewComponentResult Invoke(string name)
+        {
+            return View("~/Themes/Stisla/Components/PageAlerts/Default.cshtml", _alertManager.Alerts);
+        }
+    }
+}
